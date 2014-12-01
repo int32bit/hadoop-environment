202e31b006d7fa240bd66caaa15bddf5ab44c4d3
fgp <krystism@gmail.com>
2014-12-01 09:53:24 +0800

    add restore script

---
+++ bigFile/restore.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo cat spark-assembly-1.1.1-hadoop2.5.2.jar-* >>spark-assembly-1.1.1-hadoop2.5.2.jar
+sudo mv spark-assembly-1.1.1-hadoop2.5.2.jar ../spark/assembly/target/scala-2.10/
